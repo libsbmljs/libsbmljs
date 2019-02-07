5e7780b774a8e247f6bbf5510c241948b6bfd410
0u812 <0u812@users.noreply.github.com>
2019-02-07 14:23:32 -0800

    generate ast enum

--- tools/make_ast_enum.py
+++ tools/make_ast_enum.py
@@ -4,31 +4,30 @@ import re
 from jinja2 import Template, Environment, DictLoader
 
 jinja_env = Environment(loader=DictLoader(dict(
-    class_wrapper=\
-'''{{ docstring }}
-export class {{ symbol }} {
-{% for member in members %}
-{{ member.getDocstring() }}
-  {% if member.isStatic() %}static {% endif %}{{ member['identifier']}}() {}
+    enum_wrapper=\
+'''enum libsbml__idl__ASTNodeType_t {{ symbol }} {
+{% for value in enum_values %}
+  "libsbml::{{value}}"{{ "," if not loop.last }}
 {% endfor %}
-
-}
+};
 ''',
 )))
 jinja_env.trim_blocks = True
 jinja_env.lstrip_blocks = True
 
-
-def process_ast_enum(input_text):
+def get_enum_values(lines):
     ast_re = re.compile(r'^[\s]*,?[\s]*(AST_[\w]+)([\s*]=[\s]*[\d]+)?([\s]*/\*.*)?$')
 
     lines = input_text.splitlines()
     for line in lines:
         m = ast_re.match(line)
         if m is not None:
-            print(m.group(1))
+            yield m.group(1)
 
-    return ''
+def process_ast_enum(input_text):
+    return jinja_env.get_template('enum_wrapper').render(
+        enum_values = tuple(get_enum_values(input_text.splitlines())),
+        )
 
 
 if __name__ == '__main__':
