2c90a008c04b64be2bbb692346a36558546d96ad
0u812 <0u812@users.noreply.github.com>
2019-02-16 16:19:38 -0800

    update script

--- scripts/assign_param_return_types.py
+++ scripts/assign_param_return_types.py
@@ -7,118 +7,120 @@ class MethodDef:
         self.name = name
         self.arg_types = arg_types
 
-filepath = sys.argv[1]
-with open(filepath) as f:
-    text = f.read()
-    lines = text.splitlines()
-    n_lines = len(lines)
+for filepath in sys.argv[1:]:
+    with open(filepath) as f:
+        text = f.read()
+        lines = text.splitlines()
+        n_lines = len(lines)
 
-    arg_exp = re.compile(r'([\w]+) ([\w]+)')
-    arg_exp_ulong = re.compile(r'(unsigned long) ([\w]+)')
-    def parse_arg_types(args):
-        args = args.split(',')
-        for arg in args:
-            m = arg_exp_ulong.match(arg)
-            if m is not None:
-                yield m.group(1)
-            else:
-                m = arg_exp.match(arg)
+        arg_exp = re.compile(r'([\w]+) ([\w]+)')
+        arg_exp_ulong = re.compile(r'(unsigned long) ([\w]+)')
+        def parse_arg_types(args):
+            args = args.split(',')
+            for arg in args:
+                m = arg_exp_ulong.match(arg)
                 if m is not None:
                     yield m.group(1)
+                else:
+                    m = arg_exp.match(arg)
+                    if m is not None:
+                        yield m.group(1)
 
-    def_exp = re.compile(r'^[\s]*([\w]+) ([\w]+)\(([^)]*)\)[\s]*;[\s]*$')
-    def_exp_ulong = re.compile(r'^[\s]*(unsigned long) ([\w]+)\(([^)]*)\)[\s]*;[\s]*$')
-    def try_match_def(line):
-        m = def_exp_ulong.match(line)
-        if m is None:
-            m = def_exp.match(line)
+        def_exp = re.compile(r'^[\s]*([\w]+) ([\w]+)\(([^)]*)\)[\s]*;[\s]*$')
+        def_exp_ulong = re.compile(r'^[\s]*(unsigned long) ([\w]+)\(([^)]*)\)[\s]*;[\s]*$')
+        def try_match_def(line):
+            m = def_exp_ulong.match(line)
             if m is None:
-                return None
-        return MethodDef(
-            type=m.group(1),
-            name=m.group(2),
-            arg_types=tuple(parse_arg_types(m.group(3))),
-            )
+                m = def_exp.match(line)
+                if m is None:
+                    return None
+            return MethodDef(
+                type=m.group(1),
+                name=m.group(2),
+                arg_types=tuple(parse_arg_types(m.group(3))),
+                )
 
-    def map_method_defs(line_num, unmapped_line_nums):
-        if line_num >= len(lines):
-            return {}
-        line = lines[line_num]
-        d = try_match_def(line)
-        if d is not None:
-            return {line_num: d, **{l: d for l in unmapped_line_nums}, **map_method_defs(line_num+1, tuple())}
-        else:
-            return {**map_method_defs(line_num+1, (line_num,*unmapped_line_nums))}
-    method_def_map = map_method_defs(0, tuple())
-
-    keep_lines = {l: True for l in range(len(lines))}
-
-    def make_sig(method_def):
-        return (method_def.type,method_def.name,*method_def.arg_types)
-
-    def get_method_defs(line_num):
-        if line_num >= len(lines):
-            return {}
-        line = lines[line_num]
-        d = try_match_def(line)
-        if d is not None:
-            return {d:line_num, **get_method_defs(line_num+1)}
-        else:
-            return get_method_defs(line_num+1)
-    method_defs = get_method_defs(0)
-    sigs = {}
-    for method_def,line_num in method_defs.items():
-        sig = make_sig(method_def)
-        if not sig in sigs:
-            sigs[sig] = line_num
-        else:
-            start = sigs[sig]
-            stop = line_num
-            if stop>start:
-                for l in range(start,stop):
-                    keep_lines[l] = False
+        def map_method_defs(line_num, unmapped_line_nums):
+            if line_num >= len(lines):
+                return {}
+            line = lines[line_num]
+            d = try_match_def(line)
+            if d is not None:
+                return {line_num: d, **{l: d for l in unmapped_line_nums}, **map_method_defs(line_num+1, tuple())}
             else:
-                raise RuntimeError('Start & stop are reversed')
+                return {**map_method_defs(line_num+1, (line_num,*unmapped_line_nums))}
+        method_def_map = map_method_defs(0, tuple())
 
-    def transform_type(t):
-        if t == 'DOMString':
-            return 'string'
-        elif t == 'long':
-            return 'number'
-        elif t == 'unsigned long':
-            return 'number'
-        else:
-            return t
+        keep_lines = {l: True for l in range(len(lines))}
 
-    param_exp = re.compile(r'^([\s]*\* @param )([^{].*)$')
-    def try_transform_param(line, line_num):
-        param_match = param_exp.match(line)
-        if param_match is not None:
-            method_def = method_def_map[line_num]
-            if len(method_def.arg_types) == 1:
-                # print(param_match.group(1)+'{'+transform_type(method_def.arg_types[0])+'} '+param_match.group(2))
-                return param_match.group(1)+'{'+transform_type(method_def.arg_types[0])+'} '+param_match.group(2)
+        def make_sig(method_def):
+            return (method_def.type,method_def.name,*method_def.arg_types)
 
-    return_exp = re.compile(r'^([\s]*\* @return )([^{].*)$')
-    def try_transform_return(line, line_num):
-        m = return_exp.match(line)
-        if m is not None:
-            method_def = method_def_map[line_num]
-            return m.group(1)+'{'+transform_type(method_def.type)+'} '+m.group(2)
-
-    def transform_lines(lines):
-        for line_num,line in enumerate(lines):
-            l = try_transform_param(line, line_num)
-            if l is None:
-                l = try_transform_return(line, line_num)
-            if l is not None:
-                yield l
+        def get_method_defs(line_num):
+            if line_num >= len(lines):
+                return {}
+            line = lines[line_num]
+            d = try_match_def(line)
+            if d is not None:
+                return {d:line_num, **get_method_defs(line_num+1)}
             else:
-                yield line
+                return get_method_defs(line_num+1)
+        method_defs = get_method_defs(0)
+        sigs = {}
+        for method_def,line_num in method_defs.items():
+            sig = make_sig(method_def)
+            if not sig in sigs:
+                sigs[sig] = line_num
+            else:
+                start = sigs[sig]
+                stop = line_num
+                if stop>start:
+                    for l in range(start,stop):
+                        keep_lines[l] = False
+                else:
+                    raise RuntimeError('Start & stop are reversed')
 
-    transformed_lines = transform_lines(lines)
-    transformed_text = '\n'.join(l for n,l in enumerate(transformed_lines) if keep_lines[n])
-    # print(transformed_text)
+        def transform_type(t):
+            if t == 'DOMString':
+                return 'string'
+            elif t == 'long':
+                return 'number'
+            elif t == 'unsigned long':
+                return 'number'
+            elif t.startswith('libsbml__idl__'):
+                return t.replace('libsbml__idl__','')
+            else:
+                return t
 
-with open(filepath,'w') as f:
-    f.write(transformed_text)
+        param_exp = re.compile(r'^([\s]*\* @param )([^{].*)$')
+        def try_transform_param(line, line_num):
+            param_match = param_exp.match(line)
+            if param_match is not None:
+                method_def = method_def_map[line_num]
+                if len(method_def.arg_types) == 1:
+                    # print(param_match.group(1)+'{'+transform_type(method_def.arg_types[0])+'} '+param_match.group(2))
+                    return param_match.group(1)+'{'+transform_type(method_def.arg_types[0])+'} '+param_match.group(2)
+
+        return_exp = re.compile(r'^([\s]*\* @return )([^{].*)$')
+        def try_transform_return(line, line_num):
+            m = return_exp.match(line)
+            if m is not None:
+                method_def = method_def_map[line_num]
+                return m.group(1)+'{'+transform_type(method_def.type)+'} '+m.group(2)
+
+        def transform_lines(lines):
+            for line_num,line in enumerate(lines):
+                l = try_transform_param(line, line_num)
+                if l is None:
+                    l = try_transform_return(line, line_num)
+                if l is not None:
+                    yield l
+                else:
+                    yield line
+
+        transformed_lines = transform_lines(lines)
+        transformed_text = '\n'.join(l for n,l in enumerate(transformed_lines) if keep_lines[n])
+        # print(transformed_text)
+
+    with open(filepath,'w') as f:
+        f.write(transformed_text)
